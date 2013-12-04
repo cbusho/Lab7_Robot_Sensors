c85c3a2dbf2d87082f2c6d3140af4e4e53cc555c
Colin Busho <C15Colin.Busho@C15CRBusho.usafa.ds.af.edu>
2013-12-04 11:00:45 -0700

    First commit, includes a way to check the left right and center sensors on the robot, each function returns the ADC10MEM value

---
+++ Robot_Sensors.h
@@ -0,0 +1,19 @@
+/*
+ * Robot_Sensors.h
+ *
+ *  Created on: Dec 3, 2013
+ *      Author: C15Colin.Busho
+ */
+
+#ifndef ROBOT_SENSORS_H_
+#define ROBOT_SENSORS_H_
+
+void initSensors();
+
+unsigned int checkLeftSensor();
+
+unsigned int checkRightSensor();
+
+unsigned int checkCenterSensor();
+
+#endif /* ROBOT_SENSORS_H_ */
